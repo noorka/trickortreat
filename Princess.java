53fd9c7011543598a4f0ef62988306c494ff79e7
noorka <akroon@luc.edu>
2019-10-04 13:01:45 -0500

    updated princess
    got rid of bag and equip, made the gets public

--- Princess.java
+++ Princess.java
@@ -3,7 +3,6 @@ public class Princess {
 	private Integer speed;
 	private Integer cute;
 	private Integer scary;
-	private Integer bag;
 // constructor for a basic princess, usually you would input these values, but since we don't want that we won't
 	public Princess() {
 		this.setSpeed(3); //I can't remember what this does, but I know it is an ehh way to do things
@@ -18,7 +17,7 @@ public class Princess {
 		this.scary = scary;
 		
 	}
-	private Integer getScary() {
+	public Integer getScary() {
 		return scary;
 		
 	}
@@ -27,8 +26,8 @@ public class Princess {
 		this.cute = cute;
 		
 	}
-	private Integer getCute() {
-		return speed;
+	public Integer getCute() {
+		return cute;
 		
 	}
 
@@ -37,36 +36,8 @@ public class Princess {
 		this.speed = speed;
 		
 	}
-	private Integer getSpeed() {
+	public Integer getSpeed() {
 		return speed;
-		
-	}
-	
-// BAG
-	private void setBag(Integer bag) {
-		this.bag = bag;
-		
-	}
-	private Integer getBag() {
-		return bag;
-		
-	}
-	
-	public void equipItem(String item){
-		/* not sure what logic we want to use here, I think a switch with cases for each type of item 
-		 * then you just have to get the value into a temp variable and set it again with the temp 
-		 * Integer temp = this.getSpeed();
-		 * this.setSpeed(temp);
-		 */
-	}
-	
-	public void chooseBag(){
-		/*
-		 * So this one would have an output asking you to pick a bag then once it picks the bag we can
-		 * have ifs or switch for setting the bag type as a number 1,2,3 and re-setting whatever parameters that 
-		 * the bag choice changes. 
-		 * The thing about having this here is that we can't really affect time because it is outside the class
-		 */
 		
 	}
 	
