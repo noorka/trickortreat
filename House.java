e250399dccb0d87c0b70c9ac98adb1c49244678b
Eliora Horst <eliora.horst@gmail.com>
2019-10-05 08:20:16 +0100

    Adding random to House, MVP

--- House.java
+++ House.java
@@ -1,70 +1,83 @@
+import java.util.Random;
 
+// House Objects
 public class House {
 	private Integer cute;
 	private Integer scary;
 
-
-public House(Integer cuteFactor, Integer scaryFactor) {
-	this.setCute(cuteFactor);
-	this.setScary(scaryFactor);
-}
-private void setCute(int i) {
-	this.cute = cute;
+	// We don't need a constructor with parameters any more,
+	// Since the random house is made inside the method here
 	
-}
-public Integer getCute() {
-	return cute;
+//	public House(Integer cuteFactor, Integer scaryFactor) {
+//		this.setCute(cuteFactor);
+//		this.setScary(scaryFactor);
+//	}
 	
-}
-
-private void setScary(int i) {
-	this.scary = scary;
-	
-}
-public Integer getScary() {
-	return scary;
-	
-}
-
-//need to use the get for the costume to include it's scary and cute
-public Integer outputCandy(Integer userScary, Integer userCute ){
-	int candy = 0;
-	
-	if(scary > cute){ // scary house
-		if(userScary <= 3){
-			System.out.println("You look ... cute.");
-			candy = 1;
-		}
-		else if((userScary > 3) && (userScary <= 6)){
-			System.out.println("Nice costume.");
-			candy = 2;
-		}
-		else{
-			System.out.println("Spooktacular costume!");
-			candy = 3;
-		}
-	
+	//Cute Value of House
+	private void setCute(int i) {
+		this.cute = cute;
 	}
-	else if (cute > scary){ // cute house
-		if(userCute <= 3){
-			System.out.println("Goodness, you're scary.");
-			candy = 1;
-		}
-		else if((userCute > 3) && (userCute <= 6)){
-			System.out.println("Nice costume.");
-			candy = 2;
-		}
-		else{
-			System.out.println("You look boo-tiful!");
-			candy = 3;
-		}
-	}
-	else{ //neutral house
-		candy = 2; 
-		System.out.println("Happy Halloween!");
+	public Integer getCute() {
+		return cute;	
 	}
 	
-	
-	return candy;
-}
+	//Scary Value of House
+	private void setScary(int i) {
+		this.scary = scary;
+	}
+	public Integer getScary() {
+		return scary;	
+	}
+
+	//need to use the get for the costume to include it's scary and cute
+	public Integer outputCandy(Integer userScary, Integer userCute ){
+		int candy = 0;
+		
+		Random rand1 = new Random();
+		Random rand2 = new Random();
+		cute = rand1.nextInt(11);
+		scary = rand2.nextInt(11);
+		
+		if(scary > cute){ // scary house
+			if(userScary <= 3){
+				System.out.println("You look ... cute.");
+				candy = 1;
+				System.out.println("+1 candy");
+			}
+			else if((userScary > 3) && (userScary <= 6)){
+				System.out.println("Nice costume.");
+				candy = 2;
+				System.out.println("+2 candy");
+			}
+			else{
+				System.out.println("Spooktacular costume!");
+				candy = 3;
+				System.out.println("+3 candy");
+			}
+		
+		}
+		else if (cute > scary){ // cute house
+			if(userCute <= 3){
+				System.out.println("Goodness, you're scary.");
+				candy = 1;
+				System.out.println("+1 candy");
+			}
+			else if((userCute > 3) && (userCute <= 6)){
+				System.out.println("Nice costume.");
+				candy = 2;
+				System.out.println("+2 candy");
+			}
+			else{
+				System.out.println("You look boo-tiful!");
+				candy = 3;
+				System.out.println("+3 candy");
+			}
+		}
+		else{ //neutral house
+			candy = 2; 
+			System.out.println("Happy Halloween!");
+			System.out.println("+2 candy");
+		}
+		return candy;
+	}
 }
--- TreatMain.java
+++ TreatMain.java
@@ -9,26 +9,45 @@ import java.util.*;
 public class TreatMain {
 	public static void main(String[] args) {
 		Scanner mainInput = new Scanner(System.in);
-		int candy = 0;
+		int candy = 0, cuteness =0 , scariness=0, response = 0;
+		
 		System.out.println("Welcome to Hallowe'en Night, spooky girls and boys of all ages!");
-		
 		int choice = costumeBox();
-		
 		System.out.println("You chose: "+choice);
-		Princess costume = new Princess();
-		String response = "n";
 		
-		while(response == "n") {
-			System.out.println("Would you like to go to the next house, or go home?\n You have "+candy+" candies.");
-			response = mainInput.next();
-			if(response == "n") {
+		//This will be inside a method once we have more costumes
+		//So we can make it less specific
+		Princess costume = new Princess();
+		scariness = costume.getScary();
+		cuteness = costume.getCute();
+		
+		while(true) {
+			System.out.println("\nWhat would you like to do next?");
+			System.out.println("[1] Go to next house\n[2] Check candy levels\n[3] Go home.");
+			response = mainInput.nextInt();
+			
+			if(response == 1) {
+				House newHouse = new House();
+				candy += newHouse.outputCandy(scariness,cuteness);
+			}
+			else if(response == 2) {
+				if(candy<2) {
+					System.out.println("You have no candy.");
+				}
+				else {
+					System.out.println("You have "+candy+" candies");
+				}
+			}
+			else if(response == 3) {
 				break;
 			}
-			
-			
 		}
+		System.out.println("You end your night with "+candy+" candies.");
 	}
 	
+	
+	
+	// Get Costume Method
 	public static int costumeBox() {
 		int choice = 0;
 		Scanner userInput = new Scanner(System.in);
@@ -37,7 +56,6 @@ public class TreatMain {
 			System.out.println("[1] Princess \n[2] Superhero \n[3] Vampire");
 			choice = userInput.nextInt();
 		}
-		userInput.close();
 		return choice;
 	}
 }
