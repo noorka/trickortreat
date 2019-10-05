5bab56ec1234bafb85c531dafd1009f9fe9406ff
Eliora Horst <eliora.horst@gmail.com>
2019-10-05 22:40:16 +0100

    Adding User costume values

---
+++ sandbox.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class sandbox {
+	public static void main(String[] args) {
+		int defaultAttributes []= new int[3];
+		defaultAttributes[0]=8;
+		defaultAttributes[1]=1;
+		defaultAttributes[2]=1;
+
+		int princessAttributes []= new int[3];
+		princessAttributes[0]=3;
+		princessAttributes[1]=8;
+		princessAttributes[2]=1;
+
+		int vampireAttributes []= new int[3];
+		vampireAttributes[0]=2;
+		vampireAttributes[1]=1;
+		vampireAttributes[2]=7;
+
+		int superheroAttributes []= new int[3];
+		superheroAttributes[0]=6;
+		superheroAttributes[1]=1;
+		superheroAttributes[2]=1;
+
+		int[][] twoD_arr = {defaultAttributes, princessAttributes, vampireAttributes, superheroAttributes};
+		
+	}
+}
