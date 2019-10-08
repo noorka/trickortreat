fdf6b6a38f34d63606397813903a3527931daa32
Eliora Horst <eliora.horst@gmail.com>
2019-10-08 05:32:34 +0100

    Final pseudo code

--- TreatMVP.java
+++ TreatMVP.java
@@ -2,7 +2,7 @@
 //Fall 2019
 //Dr. Chan-Tin
 
-//My Programming Project
+//Assignment: P1 - A Program I Wrote
 //Title: Trick or Treating Console Game v.1
 //Collaborators: Eliora Horst and Anna Kroon
 //QA Testers: Bill Horst and ? Kroon
